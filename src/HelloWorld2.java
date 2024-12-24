3b9628df794a44d3d4fe6ee9a2ad957574f7b802
Yen-Yi Hsu <hsu00026@algonquinlive.com>
2024-12-23 23:58:24 -0500

    git init

---
+++ src/HelloWorld2.java
@@ -0,0 +1,19 @@
+/*
+* Student Name: Yen-Yi Hsu
+* Lab Professor: Professor Amal Ibrahim
+* Due Date: no due date
+* Modified: May, 08, 2024.
+* Description: Hybrid 01 Demo file
+*/
+
+/* Description: this class outputs a greeting from within method main. */
+public class HelloWorld2 {
+
+	/* Entry point for the application. */
+	public static void main(String[] args) {
+
+		System.out.println("Hello world");
+
+	}
+
+}
